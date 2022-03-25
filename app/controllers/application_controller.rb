b0310dde83545d3afeff5bcfa0702f827de9e829
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-24 21:23:39 -0700

    create route working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
 
 private
 
   def render_record_not_found
     render json: { error: "Record Not Found" }, status: :not_found
+  end
+  def render_record_invalid(invalid)
+    # byebug
+    render json: { error: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 end
--- app/controllers/quiz_questions_controller.rb
+++ app/controllers/quiz_questions_controller.rb
@@ -11,10 +11,17 @@ class QuizQuestionsController < ApplicationController
         render json: questions, status: :ok
     end
 
+    def create
+        new_entry = QuizQuestion.create!(question_params)
+        render json: new_entry, status: :created
+    end
     private
 
     def find_question
         QuizQuestion.find_by!(id: params[:id])
     end
-    
+
+    def question_params
+        params.permit(:question_text, :ao_1, :ao_2, :ao_3, :ao_4, :ao_5)
+    end
 end
