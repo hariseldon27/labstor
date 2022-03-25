6363b4b29ec81def09601351da639ab1ccf0c8b1
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-25 11:56:04 -0700

    in midst of getting the edit to pull from DB table

--- client/src/components/labstor/Labstor.js
+++ client/src/components/labstor/Labstor.js
@@ -1,33 +1,53 @@
 import React, {useState, useEffect} from 'react'
 import Labstor_editor from './Labstor_editor'
+import Labstor_row from './Labstor_row'
 
 function Labstor() {
+const [initialList, setInitialList] = useState([])
 const [inEdit, setInEdit] = useState([])
 
-const BASE_DB_URL = 'http://127.0.0.1:3000'
-
+// get the initial database list
 useEffect (() => {
 fetch(`http://127.0.0.1:3000/quiz_questions/`)
 .then(resp => resp.json())
-.then((data)=>setInEdit(data))
+.then((data)=>setInitialList(data))
 }, [])
 
-function handleEditChange(e){
-    const name = e.target.name;
-    let value = e.target.value;
-    setInEdit({
-        ...inEdit,
-        [name]: value,
-      });
+function saveEdit(e){
+    console.log(e)
+}
+
+function handleClickEdit(e){
+    // console.log(e.target)
+    // console.log(e.target.id)
+    fetch(`http://127.0.0.1/quiz_questions/${e.target.name}`)
+    .then(resp => resp.json())
+    .then((data) => setInEdit(data))
     
 }
 
-// console.log(inEdit)
+console.log(inEdit)
   return (
 
     <div>
         <h2>Labstor: Legend Database Editor</h2>
-        <Labstor_editor inEdit={inEdit} handleEditChange={handleEditChange} />
+        <table>
+            <thead>
+                <tr>
+                        <th>id:</th>
+                        <th>question_text:</th>
+                        <th>ao_1:</th>
+                        <th>ao_2:</th>
+                        <th>ao_3:</th>
+                        <th>ao_4:</th>
+                        <th>ao_5:</th>
+                </tr>
+            </thead>
+            <tbody>
+                <Labstor_row initialList={initialList} handleClickEdit={handleClickEdit} />
+            </tbody>
+        </table>
+        <Labstor_editor inEdit={inEdit} saveEdit={saveEdit}/>
     </div>
   )
 }
--- client/src/components/labstor/Labstor_row.js
+++ client/src/components/labstor/Labstor_row.js
@@ -1,24 +1,28 @@
 import React from 'react'
 
-function Labstor_row( { row, handleEditChange } ) {
-    // console.log(row)
+function Labstor_row( { initialList, handleClickEdit } ) {
+    // console.log(initialList)
 
-    function onEditChange(e) {
-        handleEditChange(e)
+    function onClickEdit(e) {
+        // debugger
+        console.log(e.target.name)
+        handleClickEdit(e)
     }
-    return <div>
-    <div>ID: {row.id}</div>
-    <input type="text" value={row.question_text} name="question_text" onChange={onEditChange}></input>
-    <input type="text" value={row.ao_1} name="ao_1" onChange={onEditChange}></input>
-    <input type="text" value={row.ao_2} name="ao_2" onChange={onEditChange}></input>
-    <input type="text" value={row.ao_3} name="ao_3" onChange={onEditChange}></input>
-    <input type="text" value={row.ao_4} name="ao_4" onChange={onEditChange}></input>
-    <input type="text" value={row.ao_5} name="ao_5" onChange={onEditChange}></input>
-    </div>
+    return initialList.map((row) => {
+        return ( 
+            <tr>
+                <td>{row.id}</td>
+                <td>{row.question_text}</td>
+                <td>{row.ao_1}</td>
+                <td>{row.ao_2}</td>
+                <td>{row.ao_3}</td>
+                <td>{row.ao_4}</td>
+                <td>{row.ao_5}</td>
+                <button onClick={onClickEdit} name={row.id}>&#9998;</button>
+            </tr>
 
-    // return row.map((cell) => {
-    //     return <Labstor_row cell={cell}/>
-    // })
+        )
+    })
 }
 
 export default Labstor_row
